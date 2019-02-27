5c751b4d6967d1a3034dbaeb3cce2bd38ebbe442
Aliaksei Astafyeu <lochost40@gmail.com>
2019-02-27 23:16:03 +0300

    link navigation and vector icons

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,6 +24,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+       {myIcon}
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
