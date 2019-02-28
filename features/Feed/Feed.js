da64a7cf9fab27dca050007d3e155264024d1e13
Mikhail Dziarbeyeu <mikhail_dziarbeyeu@epbyminw2832.minsk.epam.com>
2019-02-28 18:34:54 +0300

    add lint

---
+++ features/Feed/Feed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
