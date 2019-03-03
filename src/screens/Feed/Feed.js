4dcafa802f7a9eb0a9087384a6c6e94dbccc5fb8
MichDreby <mr.sincee@gmail.com>
2019-03-03 23:03:39 +0300

    complete feed(add Header + Bottom Panel)

--- src/screens/Feed/Feed.js
+++ src/screens/Feed/Feed.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 import Post from './components/Post';
+
+const image_1 = require('../../assets/post_placeholder_1.jpg');
+const image_2 = require('../../assets/post_placeholder_2.jpg');
+const image_3 = require('../../assets/post_placeholder_3.jpg');
 
 
 export default class Feed extends Component {
   render() {
     return (
-      <View>
-        <Post />
-      </View>
+      <ScrollView>
+        <Post
+          name="Bruce Wayne"
+          location="Gotham"
+          image={image_1}
+        />
+        <Post
+          name="Jim Gordon"
+          location="Gotham"
+          image={image_2}
+        />
+        <Post
+          name="Alfred Pennyworth"
+          location="Gotham"
+          image={image_3}
+        />
+      </ScrollView>
     );
   }
 }
--- src/screens/Feed/components/Post/components/BottomPanel/BottomPanel.js
+++ src/screens/Feed/components/Post/components/BottomPanel/BottomPanel.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  TouchableOpacity, View, StyleSheet,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+
+
+const styles = StyleSheet.create({
+  container: {
+    height: 56,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 8,
+  },
+  leftContainer: {
+    flexDirection: 'row',
+  },
+  icon: {
+    fontSize: 35,
+    marginRight: 5,
+  },
+});
 
 
 export default class BottomPanel extends Component {
   render() {
     return (
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: 'orange',
-        }}
-      />
+      <View style={styles.container}>
+        <View style={styles.leftContainer}>
+          <TouchableOpacity style={styles.icon}>
+            <Ionicons
+              name="ios-heart-empty"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.icon}>
+            <Feather
+              name="message-circle"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.icon}>
+            <Ionicons
+              name="md-paper-plane"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.icon}>
+          <Feather
+            name="bookmark"
+            style={[styles.icon, { marginRight: -5 }]}
+          />
+        </TouchableOpacity>
+      </View>
     );
   }
 }
